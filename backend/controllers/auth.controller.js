0611d9552b80507bdafb4561479d646284719a33
khalil-t <khaliltouil123s@gmail.com>
2025-02-23 18:58:51 +0100

    feat: add Authentication Logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,93 @@
+import User from "../model/user.model.js"
+import generateTokenAndSetCookie from "../util/generateToken.js"
 export const signup = async(req , res)=>{
-res.send("sign up route called")
+
+    try{
+        const {name ,email , password , confirmPassword} = req.body
+if (password != confirmPassword){
+    return res.status(400).json({error :'Passwords do not match' })
+}
+const find = User.findOne({email})
+
+if(find){
+    res.status(409).json({error : "Username already exists"})
+}
+const NewUser = new User({ name, email, password })
+await NewUser.save()
+const token =generateTokenAndSetCookie(NewUser._id.toString(),res)
+res.status(200).json({
+	_id: NewUser._id,
+    fullname: NewUser.fullname,
+				username: NewUser.username,
+				profilePic: NewUser.profilePic,   
+})
+    }
+    catch(error){
+        console.error('Signup error:', error);
+    
+        res.status(500).json({ error: "this" }) }
 }
 
+
+
+
+
 export const login = async(req , res)=>{
-    res.send("login up route called")
-    }
+
+try {
+const {email , password} = req.body
+
+const newuser =await User.findOne({email})
+
+generateTokenAndSetCookie(newuser._id, res )
+
+res.status(200).json({
+    _id: newuser._id,
+    fullname: newuser.fullname,
+				username: newuser.username,
+				profilePic: newuser.profilePic,
+})
+
+}
+catch(error){
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({error: "error"}) 
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     export const logout = async(req , res)=>{
-        res.send("logout route called")
-        }
+try {
+    res.clearCookie("jwt"); 
+    res.status(200).json({ message: "Logged out successfully" });
+
+}
+catch(error){
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({error: "error"})
+
+}
+
+    }
---
+++ backend/util/generateToken.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+ const generateTokenAndSetCookie =()=>{
+const token = jwt.sign({userId},process.env.JWT_SECRET,{
+    expiresIn: "15d",
+
+
+} )
+res.cookie("jwt" , token , {
+    httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+})
+
+} 
+
+export default generateTokenAndSetCookie
